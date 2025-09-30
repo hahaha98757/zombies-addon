1729ff568d5e99a391e3d7bce1f588b6014455e8
hahaha98757 <hahaha98757.2@gmail.com>
2025-09-30 20:55:30 +0900

    하위 버전 대응 브랜치

--- src/main/kotlin/kr/hahaha98757/zombiesaddon/events/GameRemoveEvent.kt
+++
@@ -1,6 +0,0 @@
-package kr.hahaha98757.zombiesaddon.events
-
-import kr.hahaha98757.zombiesaddon.game.Game
-import net.minecraftforge.fml.common.eventhandler.Event
-
-class GameRemoveEvent(val game: Game): Event()
