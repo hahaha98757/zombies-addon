2993922b55b3bd2b026b79ee88ef8584944882a9
hahaha98757 <hahaha98757.2@gmail.com>
2026-02-23 17:55:41 +0900

    타이머 보정 수정

--- src/main/kotlin/kr/hahaha98757/zombiesaddon/game/TimerCorrector.kt
+++ src/main/kotlin/kr/hahaha98757/zombiesaddon/game/TimerCorrector.kt
@@ -3,6 +3,7 @@ package kr.hahaha98757.zombiesaddon.game
 import kr.hahaha98757.zombiesaddon.utils.Scoreboard
 import kr.hahaha98757.zombiesaddon.utils.getServerNumber
 import kr.hahaha98757.zombiesaddon.utils.logger
+import kr.hahaha98757.zombiesaddon.utils.mc
 import net.minecraft.entity.monster.EntityZombie
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.event.entity.EntityJoinWorldEvent
@@ -11,6 +12,11 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 class TimerCorrector(private val game: Game) {
     @SubscribeEvent
     fun onWaveSpawn(event: EntityJoinWorldEvent) {
+        if (event.entity == mc.thePlayer) {
+            logger.info("타이머 보정을 취소합니다. (플레이어가 보정 전에 게임을 떠남)")
+            MinecraftForge.EVENT_BUS.unregister(this)
+            return
+        }
         if (event.entity !is EntityZombie) return
         if (Scoreboard.isNotZombies) return
         if (game.serverNumber == getServerNumber()) {
